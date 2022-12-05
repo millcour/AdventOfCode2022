aaf8a2d82161c16d809165c22b6056bf71113b25
Menno Pruijssers <menno@originalsuperman.nl>
2022-12-05 10:50:57 +0100

    remove days

--- src/days/day1.test.ts
+++ src/days/day1.test.ts
@@ -1,19 +1,6 @@
 import Day from './day1';
 import { dayRunner } from './test-runner';
 
-const example = `1000
-  2000
-  3000
+const example = `input`;
 
-  4000
-
-  5000
-  6000
-
-  7000
-  8000
-  9000
-
-  10000`;
-
-dayRunner(Day, example, 24000, 45000);
+dayRunner(Day, example, 1, 2);
--- src/days/day1.ts
+++ src/days/day1.ts
@@ -1,29 +1,11 @@
 import { BaseDay } from '../day';
 
 export class Day extends BaseDay<number[], number, number> {
-  parse(input: string): number[] {
-    const elves = input.split('\n\n');
-    return elves.map((elf) =>
-      elf
-        .split('\n')
-        .map((line) => parseInt(line, 10))
-        .reduce((sum, current) => sum + current, 0)
-    );
-  }
+  parse(input: string): number[] {}
 
-  async partOne(): Promise<number> {
-    const max = this.input.reduce(
-      (currentMax, elf) => Math.max(currentMax, elf),
-      0
-    );
-    return max;
-  }
+  async partOne(): Promise<number> {}
 
-  async partTwo(): Promise<number> {
-    const sorted = [...this.input].sort((a, b) => b - a);
-    const top3 = sorted.slice(0, 3);
-    return top3.reduce((sum, curr) => sum + curr, 0);
-  }
+  async partTwo(): Promise<number> {}
 }
 
 export default Day;
