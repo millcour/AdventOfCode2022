b50e28471558263737ead6a71e3cd2397c6bccf0
Menno Pruijssers <menno@originalsuperman.nl>
2022-12-08 11:51:58 +0100

    fix linting / coverage

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,5 +15,14 @@ module.exports = {
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint'],
-  rules: {},
+  rules: {
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        caughtErrorsIgnorePattern: '^_',
+      },
+    ],
+  },
 };
